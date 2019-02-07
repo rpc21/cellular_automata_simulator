3d7a58d6db14a1ba920fa2e6b1abb0c00f2334be
Dima Fayyad <dkf8D@duke.edu>
2019-02-07 16:50:57 -0500

    refactored the parseGrid method in XMLParser

--- src/RyanTester.java
+++ src/RyanTester.java
@@ -3,6 +3,6 @@ public class RyanTester {
     public static void main(String[] args) {
         System.out.println(NeighborsDefinitions.ADJACENT);
         System.out.println(NeighborsDefinitions.values());
-        System.out.println(GOLState.ALIVE.);
+        System.out.println(GOLState.ALIVE);
     }
 }
