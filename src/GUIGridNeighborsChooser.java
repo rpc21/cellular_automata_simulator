9c6cf9cfb75aca6b57762da3c99eb01df7fb7327
ard51@duke.edu <ard51@duke.edu>
2019-02-10 22:48:12 -0500

    capital neighbors

--- src/GUIGridNeighborsChooser.java
+++ src/GUIGridNeighborsChooser.java
@@ -7,8 +7,8 @@ import java.util.List;
 
 public class GUIGridNeighborsChooser extends GUIControlManager{
     private ChoiceBox<Object> myChooser = new ChoiceBox<>();
-    private List<Object> possibleNeighbors = List.of("adjacent", "box_neighbors", "diagonal", "knight", "i_formation",
-            "triangle_12_point_up", "hexagon" );
+    private List<Object> possibleNeighbors = List.of("ADJACENT", "BOX_NEIGHBORS", "DIAGNOL", "KNIGHT", "I_FORMATION",
+            "TRIANGLE_12_POINT_UP", "HEXAGON" );
 
     public GUIGridNeighborsChooser(String initialNeighbor){
         System.out.println("****" + initialNeighbor);
