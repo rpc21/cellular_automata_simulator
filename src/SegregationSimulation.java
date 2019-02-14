babd45f3d532a683b76baf37ceeabc3d7240c77b
Dima Fayyad <dkf8D@duke.edu>
2019-02-14 14:09:55 -0500

    Added comments tto classes and all public methods

--- src/SegregationSimulation.java
+++ src/SegregationSimulation.java
@@ -22,6 +22,10 @@ public class SegregationSimulation extends Simulation{
         super(params, grid);
     }
 
+    /**
+     * Determines if the simulation is ready to be ended
+     * @return true when the simulation is over
+     */
     @Override
     public boolean isOver() {
         for (Cell cell: myGrid.getCells()){
@@ -32,16 +36,31 @@ public class SegregationSimulation extends Simulation{
         return true;
     }
 
+    /**
+     * Used to get the string value of the name of the simulation
+     * Used to keep consistency in simulation naming
+     * @return the string value of the name of the simulation
+     */
     @Override
     public String getMyName() {
         return Simulation.SEGREGATION_SIMULATION_NAME;
     }
 
+    /**
+     * Gets the fields in the simulation data fields that determine percentages of cell types
+     * Used when the initial states for the simulation should be determine based on the specified
+     * population percentages
+     * @return fields in the simulation data fields that determine percentages of cell types
+     */
     @Override
     public List<String> getPercentageFields() {
         return List.of(RED_PERCENTAGE, BLUE_PERCENTAGE, EMPTY_PERCENTAGE);
     }
 
+    /**
+     * Used when the user changes style properties from the GUI
+     * @param styleProperties the new style properties: the neighbors type, grid line option, shape, edge type for grid
+     */
     @Override
     public void updateNeighbors(Map<String, String> styleProperties){
         super.updateNeighbors(styleProperties, NeighborsDefinitions.BOX_NEIGHBORS);
